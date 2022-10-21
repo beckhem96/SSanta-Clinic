e102ba4030494415e0d9c83b873228596cca8d37
dongwhan <ldh793@naver.com>
2022-10-21 16:10:09 +0900

    (:sparkles:) feat : User Entity 생성

---
+++ Backend/src/main/java/com/ssafy/ssantaClinic/db/entity/User.java
@@ -0,0 +1,29 @@
+package com.ssafy.ssantaClinic.db.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickName;
+
+}
