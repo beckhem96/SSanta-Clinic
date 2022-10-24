7e476dc6d8180bf4980b584174f3da8148ca2417
dongwhan <ldh793@naver.com>
2022-10-24 14:53:48 +0900

    :sparkles: feat : 회원가입/상세, DB정보 암호화

--- Backend/src/main/java/com/ssafy/ssantaClinic/api/service/UserServiceImpl.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/api/service/UserServiceImpl.java
@@ -5,14 +5,24 @@ import com.ssafy.ssantaClinic.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService{
 
-    @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @Override
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    @Override
+    public User getUserByUserId(int userId) {
+        User user = userRepository.getUserByUserId(userId);
+        return user;
     }
 }
