c954831e7cccb8a62477fd72a7b564aee2334cef
허성은 <aysel0230@gmail.com>
2022-10-24 16:55:49 +0900

    :sparkles: feat : 어드벤트 캘린더 repository 생성

---
+++ Backend/src/main/java/com/ssafy/ssantaClinic/db/repository/AdventCalendarRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.ssantaClinic.db.repository;
+
+import com.ssafy.ssantaClinic.db.entity.AdventCalendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdventCalendarRepository extends JpaRepository<AdventCalendar, Integer> {
+    List<AdventCalendar> findAllByReceiverIdAndDay()
+}
