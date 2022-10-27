2533bf7a15cf4e5b1396438be6c0e0fdfba749b8
yoonkyu <newjeff@naver.com>
2022-10-27 15:47:55 +0900

    :sparkles: feat: 순발력게임(기능구현, 스타일 x)

--- Frontend/ssantaclinic/src/App.tsx
+++ Frontend/ssantaclinic/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignUp } from './pages/users/SignUp';
 import { LogIn } from './pages/users/LogIn';
 import { Home } from './pages/Home';
+import WitsPage from './pages/game/WitsPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/wits" element={<WitsPage />}></Route>
       </Routes>
     </Router>
   );
