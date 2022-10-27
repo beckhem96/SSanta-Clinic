ea17227a8f50ce2904a5cc482241fb63b107f6d8
Your Name <beckhem96@naver.com>
2022-10-27 16:03:26 +0900

    :bug: fix: 비상사태라 한번 develop으로 머지할게요

--- Frontend/ssantaclinic/src/App.tsx
+++ Frontend/ssantaclinic/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SignUp } from './pages/users/SignUp';
-import { LogIn } from './pages/users/LogIn';
+import { SignUp } from './pages/users/SignUpPage';
+import { LogIn } from './pages/users/LogInPage';
 import { Home } from './pages/Home';
 import WitsPage from './pages/game/WitsPage';
-import { FindPassword } from './pages/users/FindPassword';
-import { ChangePassword } from './pages/users/ChangePassword';
-
+import { FindPassword } from './pages/users/FindPasswordPage';
+import { ChangePassword } from './pages/users/ChangePasswordPage';
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <Route path="/wits" element={<WitsPage />}></Route>
         <Route path="/findpassword" element={<FindPassword />}></Route>
         <Route path="/changepassword" element={<ChangePassword />}></Route>
-
       </Routes>
     </Router>
   );
