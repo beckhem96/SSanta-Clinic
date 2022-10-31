0837fad017ecd245330cc9dc6e832f630bc282d8
허성은 <aysel0230@gmail.com>
2022-10-31 15:59:56 +0900

    :lipstick: style : 알림 컨트롤러 주석 추가
    Former-commit-id: 7378e868182d54bca737f8933420e0365a91d9b4

--- Backend/src/main/java/com/ssafy/ssantaClinic/api/controller/NotiController.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/api/controller/NotiController.java
@@ -1,15 +1,21 @@
 package com.ssafy.ssantaClinic.api.controller;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @FileName : NotiController
+ * @Class 설명 : 알림 관련 요청을 처리하는 Controller
+ */
+@Api(value = "알림 관련 API", tags = {"NotiController"}, description = "알림 관련 컨트롤러")
 @RestController
 @Slf4j
 @CrossOrigin(origins = "*")
 @RequestMapping("/noti")
 @RequiredArgsConstructor
 public class NotiController {
+
 }
