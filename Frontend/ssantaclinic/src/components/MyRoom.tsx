9cdbdb89d1f002da0f64945026f5a0f4f2df9559
Your Name <beckhem96@naver.com>
2022-11-01 17:56:06 +0900

    :sparkles: feat: router 파라미터 코드 추가, recoil 패키지 추가
    Former-commit-id: 02d99d7fcb9835c472eeb28ba4bc4feb3ab9a80a

--- Frontend/ssantaclinic/src/components/MyRoom.tsx
+++ Frontend/ssantaclinic/src/components/MyRoom.tsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import { selectToken } from '../store/store';
 import { selectUserId, selectUserNickname } from '../store/store';
 import { useRecoilValue } from 'recoil';
 import { useParams } from 'react-router-dom';
 
 export const MyRoom = () => {
   const { id } = useParams(); // const 변수명 = useParams().파라미터명;
-  const [tree, setTree] = useState<string>('');
-  const [calendar, setCalendar] = useState<string>('');
+
   // const TOKEN = useRecoilValue(selectToken);
   const NICKNAME = useRecoilValue(selectUserNickname);
   const ID = useRecoilValue(selectUserId);
   const TOKEN = localStorage.getItem('jwt') || '';
+
   useEffect(() => {
     axios
-      .get('http://localhost:8080' + '/api/room/14', {})
+      .get('http://localhost:8080/api/room/' + id, {
+        headers: {
+          Authorization: TOKEN,
+        },
+      })
       .then((res) => {
         console.log(res.data);
-        setTree('내 트리');
-        setCalendar('내 캘ㄹ;ㄴ더');
       })
       .catch((err) => {
         console.log(err.resonse);
@@ -32,10 +33,7 @@ export const MyRoom = () => {
         안녕하세요 {ID}번 째 유저인 {NICKNAME}님
       </h2>
       <h3>당신의 토큰은 {TOKEN}이네요 야미~</h3>
-      <div>
-        <h2>{tree}</h2>
-        <h2>{calendar}</h2>
-      </div>
+      <div></div>
     </div>
   );
 };
