3ce8509f9349e7de512a6e2013e280761205194f
Your Name <beckhem96@naver.com>
2022-11-01 13:46:53 +0900

    :sparkles: feat: JWT localstroage에 일단 저장
    Former-commit-id: 96109fc036c7abb51c682c54196f9c643d21b52f

--- Frontend/ssantaclinic/src/store/store.tsx
+++ Frontend/ssantaclinic/src/store/store.tsx
@@ -2,28 +2,46 @@ import { atom, selector } from 'recoil';
 
 export interface IUser {
   email: string;
-  token: string;
+  id: string;
   nickname: string;
+  // jwt: string;
 }
 
 export const currentUser = atom<IUser>({
   key: 'user',
   default: {
     email: '',
-    token: '',
+    id: '',
     nickname: '',
+    // jwt: '',
   },
 });
 
-export const selectToken = selector<string>({
-  key: 'nowUserToken',
+// export const selectToken = selector({
+//   key: 'userToken',
+//   get: ({ get }) => {
+//     const user = get(currentUser);
+//     return user.jwt;
+//   },
+// });
+
+export const selectEmail = selector<string>({
+  key: 'nowUserEmail',
   get: ({ get }) => {
     const user = get(currentUser);
-    return user.token;
+    return user.email;
   },
 });
 
-export const selectNickname = selector<string>({
+export const selectUserId = selector<string>({
+  key: 'nowUserId',
+  get: ({ get }) => {
+    const user = get(currentUser);
+    return user.id;
+  },
+});
+
+export const selectUserNickname = selector<string>({
   key: 'nowUserNickname',
   get: ({ get }) => {
     const user = get(currentUser);
