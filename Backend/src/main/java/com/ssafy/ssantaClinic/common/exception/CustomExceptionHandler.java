52e758da084b700e12384cc0dea95249b5a4d058
허성은 <aysel0230@gmail.com>
2022-11-02 12:51:17 +0900

    :hammer: chore : 콘솔에 오류 로그 띄우기
    Former-commit-id: 960ae6c4da304ab047eb49d8051978bfb05ff48a

--- Backend/src/main/java/com/ssafy/ssantaClinic/common/exception/CustomExceptionHandler.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/common/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssantaClinic.common.exception;
 
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
+        e.printStackTrace();
         return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
     }
 }
