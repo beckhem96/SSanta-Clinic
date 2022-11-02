c67702564130445b5405db8207c5c5e949c337e1
김혜린 <herrine.kim@gmail.com>
2022-11-02 13:49:03 +0900

    ✨ feat : 상자 보내기 구현 완료
    Former-commit-id: df095a1a35a95dd7ad66ceb91a5f7086442c5687

--- Frontend/ssantaclinic/src/pages/calendar/boxCreate/index.tsx
+++ Frontend/ssantaclinic/src/pages/calendar/boxCreate/index.tsx
@@ -1,43 +1,78 @@
-import React, { useCallback, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { ReactMediaRecorder } from 'react-media-recorder';
-// https://stackoverflow.com/questions/56457935/typescript-error-property-x-does-not-exist-on-type-window
-// declare const window: any;
-// const ACCESS_TOKEN = localStorage.getItem('jwt') || '';
+const ACCESS_TOKEN = localStorage.getItem('jwt') || '';
 export function BoxCreate() {
   // 상자 보내기 (/api/calendar)
   const [audio, setAudio] = useState<File>();
+  const [image, setImage] = useState<File[]>([]);
+  const [content, setContent] = useState<string>('');
+  const [createdAt, setCreatedAt] = useState<string>('');
+  const [day, setDay] = useState<number>(1);
+  const [receiverId, setReceiverId] = useState<number>(1);
+
+  // 자동으로 현재 날짜 및 시간 yyyy-mm-dd hh:mm:ss 형태로 가져오기
+  useEffect(() => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    // 만약에 month, day, hour, minute, second가 한 자리 수라면 앞에 0을 붙인다
+    const monthString = month < 10 ? `0${month}` : month;
+    const dayString = day < 10 ? `0${day}` : day;
+    const hourString = hour < 10 ? `0${hour}` : hour;
+    const minuteString = minute < 10 ? `0${minute}` : minute;
+    const secondString = second < 10 ? `0${second}` : second;
+    const dateString = `${year}-${monthString}-${dayString} ${hourString}:${minuteString}:${secondString}`;
+    setCreatedAt(dateString);
+  }, []);
+
   const sendBox = (e: any) => {
     e.preventDefault();
-    const formData = new FormData();
-    if (audio) {
-      formData.append('audio', audio);
-    }
+    // boxRequest를 json 형태로 담아 준다.
     const boxRequest = {
-      content: 'content',
-      createdAt: '2022-11-01',
-      day: 1,
-      receiverId: 1, // 받는 사람 아이디
+      content: content,
+      sender: 1,
+      createdAt: createdAt,
+      day: day,
+      receiverId: receiverId,
     };
-    const blob = new Blob([JSON.stringify(boxRequest)], {
+    // formData를 생성한다.
+    // https://stackoverflow.com/questions/55101841/argument-of-type-file-is-not-assignable-to-parameter-of-type-string-blob-i
+    const boxFormData: any = new FormData();
+    // boxRequest를 formData에 담는다.
+    const boxBlob = new Blob([JSON.stringify(boxRequest)], {
       type: 'application/json',
     });
-    formData.append('boxRequest', blob);
-    axios
-      .post('http://localhost:8080' + '/api/calendar', {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhcHJpbGtpbTk4QG5hdmVyLmNvbSIsImF1dGgiOiIiLCJleHAiOjE2NjcyOTM0MDh9.xtnddbu6dWYdBVqAeaU6HCnyf-dex9aavcuEqCNrwwBaQ22Z6RUQa8IMzfSAu0M8i2E3A-Oc_udfKbdIvJkdkA',
-          'Content-type': 'multipart/form-data',
-        },
-        data: formData,
-      })
+    boxFormData.append('boxRequest', boxBlob);
+    // audio가 있으면 formData에 담는다.
+    if (audio) {
+      boxFormData.append('audio', audio);
+    }
+    // image가 있으면 formData에 담는다.
+    if (image) {
+      boxFormData.append('imges', image);
+    }
+    // config
+    const config = {
+      method: 'post',
+      url: 'http://localhost:8080/api/calendar',
+      headers: {
+        Authorization: ACCESS_TOKEN,
+        'Content-Type': 'multipart/form-data',
+      },
+      data: boxFormData,
+    };
+
+    axios(config)
       .then((res) => {
-        console.log('응답 받아옴 성공!', res.data);
+        console.log(res);
       })
       .catch((err) => {
-        // console.log(ACCESS_TOKEN);
-        console.log(err.response);
+        console.log(err);
       });
   };
   return (
@@ -50,23 +85,30 @@ export function BoxCreate() {
             <button onClick={startRecording}>Start Recording</button>
             <button onClick={stopRecording}>Stop Recording</button>
             <video src={mediaBlobUrl} controls autoPlay loop />
-            {/* 파일 형태로 저장 */}
+            {/* 내가 보내려는 음성을 저장 */}
             <button
               onClick={() => {
                 // Url을 File로 변환하여 저장
                 if (mediaBlobUrl) {
-                  const file = new File([mediaBlobUrl], 'audio.wav', {
-                    type: 'audio/wav',
-                  });
-                  setAudio(file);
-                  console.log(typeof file);
+                  // setAudio
+                  fetch(mediaBlobUrl)
+                    .then((res) => res.blob())
+                    .then((blob) => {
+                      const file = new File([blob], 'audio_message.wav', {
+                        type: 'audio_message/wav',
+                      });
+                      setAudio(file);
+                    })
+                    .then(() => {
+                      console.log(audio);
+                    });
                 }
               }}
             >
               저장
             </button>
             {/* 다운로드 */}
-            <a href={mediaBlobUrl} download="my-audio-file.mp3">
+            <a href={mediaBlobUrl} download="my-audio-file.wav">
               Download
             </a>
             {/* 상자 보내기 */}
@@ -74,112 +116,34 @@ export function BoxCreate() {
           </div>
         )}
       />
+      {/* content */}
+      <input
+        type="text"
+        placeholder="content"
+        onChange={(e) => setContent(e.target.value)}
+      />
+      {/* day */}
+      <input
+        type="number"
+        placeholder="day"
+        onChange={(e) => setDay(Number(e.target.value))}
+      />
+      {/* receiverId */}
+      <input
+        type="number"
+        placeholder="receiverId"
+        onChange={(e) => setReceiverId(Number(e.target.value))}
+      />
+      {/* image */}
+      <input
+        type="file"
+        multiple
+        onChange={(e) => {
+          if (e.target.files) {
+            setImage(Array.from(e.target.files));
+          }
+        }}
+      />
     </div>
   );
 }
-// // 음성 녹음 기능
-// const [stream, setStream] = useState<any>();
-// const [media, setMedia] = useState<any>();
-// const [onRec, setOnRec] = useState<boolean>(true);
-// const [source, setSource] = useState<any>();
-// const [analyser, setAnalyser] = useState<any>();
-// const [audioUrl, setAudioUrl] = useState<any>();
-
-// const onRecAudio = () => {
-//   // 음원 정보를 담은 노드를 생성하거나 음원을 실행 또는 디코딩
-//   const audioContext = new (window.AudioContext ||
-//     window.webkitAudioContext)();
-//   // Javascript를 통해 음원의 진행상태에 직접 접근한다.
-//   const analyser = audioContext.createScriptProcessor(0, 1, 1);
-//   setAnalyser(analyser);
-
-//   function makeSound(stream: any) {
-//     // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여 줌
-//     const source = audioContext.createMediaStreamSource(stream);
-//     setSource(source);
-//     source.connect(analyser);
-//     analyser.connect(audioContext.destination);
-//   }
-
-//   // 마이크 사용 권한 획득
-//   navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-//     const mediaRecorder = new MediaRecorder(stream);
-//     mediaRecorder.start();
-//     setStream(stream);
-//     setMedia(mediaRecorder);
-//     makeSound(stream);
-
-//     analyser.onaudioprocess = function (e: any) {
-//       // 1분(60초) 지나면 자동으로 음성 저장 및 녹음 중지
-//       if (e.playbackTime > 60) {
-//         stream.getAudioTracks().forEach((track: any) => track.stop());
-//         mediaRecorder.stop();
-//         // 메서드가 호출된 노드 연결 해제
-//         analyser.disconnect();
-//         audioContext.createMediaStreamSource(stream).disconnect();
-
-//         mediaRecorder.ondataavailable = function (e: any) {
-//           setAudioUrl(e.data);
-//           setOnRec(true);
-//         };
-//       } else {
-//         setOnRec(false);
-//       }
-//     };
-//   });
-// };
-
-// // 사용자가 음성 녹음을 중지했을 때
-// const offRecAudio = () => {
-//   // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
-//   media.ondataavailable = function (e: any) {
-//     setAudioUrl(e.data);
-//     setOnRec(true);
-//   };
-
-//   // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
-//   stream.getAudioTracks().forEach((track: any) => track.stop());
-
-//   // 미디어 캡쳐 중지
-//   media.stop();
-//   // 메서드가 호출된 노드 연결 해제
-//   analyser.disconnect();
-//   source.disconnect();
-// };
-
-// const onSubmitAudioFile = useCallback(() => {
-//   if (audioUrl) {
-//     console.log(URL.createObjectURL(audioUrl)); // 출력된 링크에서 녹음된 오디오 확인 가능
-//   }
-//   // File 생성자를 사용해 파일로 변환
-//   const sound = new File([audioUrl], 'soundBlob', {
-//     lastModified: new Date().getTime(),
-//     type: 'audio',
-//   });
-//   console.log(sound); // File 정보 출력
-// }, [audioUrl]);
-
-// // 녹음 파일 다운로드
-// const downloadAudioFile = useCallback(() => {
-//   if (audioUrl) {
-//     const url = URL.createObjectURL(audioUrl);
-//     const a = document.createElement('a');
-//     document.body.appendChild(a);
-//     a.style.display = 'none';
-//     a.href = url;
-//     a.download = 'soundBlob';
-//     a.click();
-//     window.URL.revokeObjectURL(url);
-//   }
-// }, [audioUrl]);
-
-//   return (
-//     <div>
-//       <h1>BoxCreate</h1>
-//       <button onClick={onRec ? onRecAudio : offRecAudio}>녹음</button>
-//       <button onClick={onSubmitAudioFile}>결과 확인</button>
-//       {/* 다운로드 */}
-//       <button onClick={downloadAudioFile}>다운로드</button>
-//     </div>
-//   );
-// }
