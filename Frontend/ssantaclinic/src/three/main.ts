9a1fe2baa0db26b8b8384650926980fdf71a6afa
yoonkyu <newjeff@naver.com>
2022-11-02 14:57:26 +0900

    :sparkles: feat : zoomout 추가
    zoomout 경로가 조금 이상함. 수정필요
    
    
    Former-commit-id: f0a3d30cb551ae3742ff5285c17030f9c417d206

--- Frontend/ssantaclinic/src/three/main.ts
+++ Frontend/ssantaclinic/src/three/main.ts
@@ -473,6 +473,7 @@ export class MainCanvas {
       if (targets[0].object.name === 'tree') {
         console.log('tree!!!!!');
         // 트리 줌인 후에 꾸밀수 있도록 인벤토리
+        this._zoomFit(targets[0].object.parent, 60);
       } else if (targets[0].object.name === 'house') {
         console.log('house!!!!!!!!');
         this._zoomFit(targets[0].object.parent, 60);
@@ -484,13 +485,13 @@ export class MainCanvas {
       } else {
         this._removeModal();
         setTimeout(() => {
-          this._setupCamera();
+          this._zoomFit(this._model, 60);
         }, 100);
       }
     } else {
       this._removeModal();
       setTimeout(() => {
-        this._setupCamera();
+        this._zoomFit(this._model, 60);
       }, 100);
     }
   }
