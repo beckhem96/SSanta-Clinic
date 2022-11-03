af19a8c7082c8260ee6a58041ed920fbc4aa427e
herrinekim <herrine.kim@gmail.com>
2022-11-03 09:56:59 +0900

    :bug: fix : tetris props type 문제 해결
    Former-commit-id: 5740b5c2dd7a25d19c53b8dee5aa498f51ea3843

--- Frontend/ssantaclinic/src/components/game/tetris/Tetrominos.tsx
+++ Frontend/ssantaclinic/src/components/game/tetris/Tetrominos.tsx
@@ -1,4 +1,4 @@
-export const TETRIMINOS: any = {
+export const TETROMINOS: any = {
   0: { shape: [[0]], color: '0, 0, 0' },
   I: {
     shape: [
@@ -59,8 +59,8 @@ export const TETRIMINOS: any = {
 };
 
 export const randomTetrimino = () => {
-  const tetriminos = 'IJLOSTZ';
-  const randTetrimino: string =
-    tetriminos[Math.floor(Math.random() * tetriminos.length)];
-  return TETRIMINOS[randTetrimino];
+  const tetrominos = 'IJLOSTZ';
+  const randTetromino: string =
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
+  return TETROMINOS[randTetromino];
 };
