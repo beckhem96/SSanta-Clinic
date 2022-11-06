c69aa361b85a009e065c11b0dc533a2eee29294f
herrinekim <herrine.kim@gmail.com>
2022-11-06 19:12:21 +0900

    :hammer: chore : MUI 설치
    Former-commit-id: 89d13895d481262a0750d4bf9db0cf6ce368cefa

--- Frontend/ssantaclinic/src/pages/calendar/calendarPage/index.tsx
+++ Frontend/ssantaclinic/src/pages/calendar/calendarPage/index.tsx
@@ -3,19 +3,67 @@ import { CalendarPageContainer } from './styles';
 import { CalendarPageTitle } from './styles';
 import { AdventCalendarBox } from './styles';
 import { CalendarDetail } from '../../../components/calendar/calendarDetail';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export function CalendarPage() {
   // 모달창 노출 여부
-  const [calendarOpen, setCalendarOpen] = React.useState<boolean>(false);
-  // 모달창 노출
-  const showCalendar = () => {
-    setCalendarOpen(true);
-  };
+  // const [calendarOpen, setCalendarOpen] = React.useState<boolean>(false);
+  // // 모달창 노출
+  // const showCalendar = () => {
+  //   setCalendarOpen(true);
+  // };
   return (
-    <CalendarPageContainer>
-      <CalendarPageTitle>어드벤트 캘린더 페이지</CalendarPageTitle>
-      <AdventCalendarBox onClick={showCalendar}>day n</AdventCalendarBox>
-      {calendarOpen && <CalendarDetail setCalendarOpen={setCalendarOpen} />}
-    </CalendarPageContainer>
+    // <CalendarPageContainer>
+    //   <CalendarPageTitle>어드벤트 캘린더 페이지</CalendarPageTitle>
+    //   <AdventCalendarBox onClick={showCalendar}>day n</AdventCalendarBox>
+    //   {calendarOpen && <CalendarDetail setCalendarOpen={setCalendarOpen} />}
+    // </CalendarPageContainer>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container justifyContent="center" spacing={3} columns={9}>
+        <Grid direction="column" item xs={4}>
+          <Grid item xs={2}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={2}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+        <Grid direction="column" item xs={1}>
+          <Grid direction="column" container columns={8}>
+            <Grid item xs={4}>
+              <Item>xs=8</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=8</Item>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid direction="column" item xs={4}>
+          <Grid item xs={2}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={2}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>{' '}
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
