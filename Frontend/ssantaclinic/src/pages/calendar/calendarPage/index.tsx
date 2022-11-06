422aae6c51baa8b8a3e2e0f9f2e0e03a05ff2d03
herrinekim <herrine.kim@gmail.com>
2022-11-06 23:22:03 +0900

    :sparkles: feat : 어드벤트 캘린더 flexbox 구현
    Former-commit-id: 5bd8eb823ad03991e79116d4d4c9b41128fa4fe9

--- Frontend/ssantaclinic/src/pages/calendar/calendarPage/index.tsx
+++ Frontend/ssantaclinic/src/pages/calendar/calendarPage/index.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import { CalendarPageContainer } from './styles';
-import { CalendarPageTitle } from './styles';
-import { AdventCalendarBox } from './styles';
-import { CalendarDetail } from '../../../components/calendar/calendarDetail';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import { CalendarLeftContainer } from './styles';
+// 좌측 상단
+import { MiniContainerOne } from './styles';
+import { BoxOne } from './styles';
+import { BoxTwo } from './styles';
+import { BoxThree } from './styles';
+// 좌측 중간
+import { MiniContainerTwo } from './styles';
+import { BoxEight } from './styles';
+import { MiniContainerTwoRight } from './styles';
+import { MiniContainerTwoTop } from './styles';
+import { BoxNine } from './styles';
+import { BoxTen } from './styles';
+import { MiniContainerTwoBottom } from './styles';
+import { BoxThirteen } from './styles';
+import { BoxFourteen } from './styles';
+import { BoxFifteen } from './styles';
+// 좌측 하단
+import { MiniContainerThree } from './styles';
+import { BoxTwentyOne } from './styles';
+import { BoxTwentyTwo } from './styles';
+import { BoxTwentyThree } from './styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import { CalendarMiddleContainer } from './styles';
+import { BoxFour } from './styles';
+import { BoxSixteen } from './styles';
+import { CalendarRightContainer } from './styles';
+// 우측 상단
+import { MiniContainerFour } from './styles';
+import { BoxFive } from './styles';
+import { BoxSix } from './styles';
+import { BoxSeven } from './styles';
+// 우측 중간
+import { MiniContainerFive } from './styles';
+import { BoxEleven } from './styles';
+import { BoxTwelve } from './styles';
+// 우측 하단
+import { MiniContainerSix } from './styles';
+import { BoxSeventeen } from './styles';
+import { BoxTwentyFour } from './styles';
+import { MiniContainerSeven } from './styles';
+import { MiniContainerEight } from './styles';
+import { MiniContainerNine } from './styles';
+import { BoxNineteen } from './styles';
+import { BoxTwentyFive } from './styles';
+import { MiniContainerTen } from './styles';
 
 export function CalendarPage() {
   // 모달창 노출 여부
@@ -24,46 +55,60 @@ export function CalendarPage() {
   //   setCalendarOpen(true);
   // };
   return (
-    // <CalendarPageContainer>
-    //   <CalendarPageTitle>어드벤트 캘린더 페이지</CalendarPageTitle>
-    //   <AdventCalendarBox onClick={showCalendar}>day n</AdventCalendarBox>
-    //   {calendarOpen && <CalendarDetail setCalendarOpen={setCalendarOpen} />}
-    // </CalendarPageContainer>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container justifyContent="center" spacing={3} columns={9}>
-        <Grid direction="column" item xs={4}>
-          <Grid item xs={2}>
-            <Item>xs=8</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=8</Item>
-          </Grid>
-          <Grid item xs={2}>
-            <Item>xs=8</Item>
-          </Grid>
-        </Grid>
-        <Grid direction="column" item xs={1}>
-          <Grid direction="column" container columns={8}>
-            <Grid item xs={4}>
-              <Item>xs=8</Item>
-            </Grid>
-            <Grid item xs={4}>
-              <Item>xs=8</Item>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid direction="column" item xs={4}>
-          <Grid item xs={2}>
-            <Item>xs=8</Item>
-          </Grid>
-          <Grid item xs={2}>
-            <Item>xs=8</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=8</Item>
-          </Grid>{' '}
-        </Grid>
-      </Grid>
-    </Box>
+    <CalendarPageContainer>
+      <CalendarLeftContainer>
+        <MiniContainerOne>
+          <BoxOne>1</BoxOne>
+          <BoxTwo>2</BoxTwo>
+          <BoxThree>3</BoxThree>
+        </MiniContainerOne>
+        <MiniContainerTwo>
+          <BoxEight>8</BoxEight>
+          <MiniContainerTwoRight>
+            <MiniContainerTwoTop>
+              <BoxNine>9</BoxNine>
+              <BoxTen>10</BoxTen>
+            </MiniContainerTwoTop>
+            <MiniContainerTwoBottom>
+              <BoxThirteen>13</BoxThirteen>
+              <BoxFourteen>14</BoxFourteen>
+              <BoxFifteen>15</BoxFifteen>
+            </MiniContainerTwoBottom>
+          </MiniContainerTwoRight>
+        </MiniContainerTwo>
+        <MiniContainerThree>
+          <BoxTwentyOne>21</BoxTwentyOne>
+          <BoxTwentyTwo>22</BoxTwentyTwo>
+          <BoxTwentyThree>23</BoxTwentyThree>
+        </MiniContainerThree>
+      </CalendarLeftContainer>
+      <CalendarMiddleContainer>
+        <BoxFour></BoxFour>
+        <BoxSixteen></BoxSixteen>
+      </CalendarMiddleContainer>
+      <CalendarRightContainer>
+        <MiniContainerFour>
+          <BoxFive>5</BoxFive>
+          <BoxSix>6</BoxSix>
+          <BoxSeven>7</BoxSeven>
+        </MiniContainerFour>
+        <MiniContainerFive>
+          <BoxEleven>11</BoxEleven>
+          <BoxTwelve>12</BoxTwelve>
+        </MiniContainerFive>
+        <MiniContainerSix>
+          <MiniContainerSeven>
+            <BoxSeventeen>17</BoxSeventeen>
+            <BoxTwentyFour>24</BoxTwentyFour>
+          </MiniContainerSeven>
+          <MiniContainerEight>18</MiniContainerEight>
+          <MiniContainerNine>
+            <BoxNineteen>19</BoxNineteen>
+            <BoxTwentyFive>25</BoxTwentyFive>
+          </MiniContainerNine>
+          <MiniContainerTen>20</MiniContainerTen>
+        </MiniContainerSix>
+      </CalendarRightContainer>
+    </CalendarPageContainer>
   );
 }
--- Frontend/ssantaclinic/src/pages/calendar/calendarPage/styles.tsx
+++ Frontend/ssantaclinic/src/pages/calendar/calendarPage/styles.tsx
@@ -2,44 +2,385 @@ import styled from 'styled-components';
 
 export const CalendarPageContainer = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 85vh;
+  width: 92vw;
+  background-color: skyblue;
+`;
+
+// 좌측 영역
+export const CalendarLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 80vh;
+  width: 40vw;
+  background-color: pink;
+`;
+// 좌측 상단 미니 컨테이너
+export const MiniContainerOne = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 20vh;
+  width: 40vw;
+  background-color: orange;
+`;
+
+export const BoxOne = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const BoxTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 18vw;
+  background-color: white;
+`;
+
+export const BoxThree = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+// 좌측 중간 미니 컨테이너
+export const MiniContainerTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 40vh;
+  width: 40vw;
+  background-color: brown;
+`;
+
+export const BoxEight = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 38vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const MiniContainerTwoRight = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
+  height: 40vh;
+  width: 31vw;
+  background-color: white;
+`;
+
+export const MiniContainerTwoTop = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 20vh;
+  width: 31vw;
+  background-color: white;
+`;
+
+export const BoxNine = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const BoxTen = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 18vw;
+  background-color: yellow;
+`;
+
+export const MiniContainerTwoBottom = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 20vh;
+  width: 31vw;
+  background-color: white;
+`;
+
+export const BoxThirteen = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const BoxFourteen = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const BoxFifteen = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: yellow;
+`;
+
+export const MiniContainerThree = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 20vh;
+  width: 40vw;
+  background-color: mintcream;
+`;
+
+export const BoxTwentyOne = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 18vw;
+  background-color: blue;
+`;
+
+export const BoxTwentyTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: blue;
+`;
+
+export const BoxTwentyThree = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 18vh;
+  width: 9vw;
+  background-color: blue;
+`;
+
+// 가운데 영역
+export const CalendarMiddleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 80vh;
+  width: 10vw;
+  background-color: yellowgreen;
+`;
+
+export const BoxFour = styled.div`
+  align-items: center;
   justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #f5f5f5;
+  height: 39vh;
+  width: 9vw;
+  background-color: green;
 `;
 
-export const CalendarPageTitle = styled.h1`
-  font-size: 2rem;
-  font-weight: 700;
-  color: #333;
+export const BoxSixteen = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 39vh;
+  width: 9vw;
+  background-color: darkgreen;
 `;
 
-export const CalendarPageSubTitle = styled.h2`
-  font-size: 1.5rem;
-  font-weight: 700;
-  color: #333;
-`;
-
-export const AdventCalendarBox = styled.div`
+// 우측 영역
+export const CalendarRightContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100px;
-  width: 100px;
+  height: 80vh;
+  width: 40vw;
+  background-color: yellow;
+`;
+
+// 우측 상단 미니 영역
+export const MiniContainerFour = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 20vh;
+  width: 40vw;
+  background-color: orange;
+`;
+
+export const BoxFive = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
+  background-color: blue;
+`;
+
+export const BoxSix = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 19vw;
+  background-color: darkblue;
+`;
+
+export const BoxSeven = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
   background-color: red;
 `;
 
-export const CalendarPageButton = styled.button`
-  font-size: 1rem;
-  font-weight: 400;
-  color: #333;
-  background-color: #f5f5f5;
-  border: 1px solid #333;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  margin: 0.5rem;
-  cursor: pointer;
+// 우측 중간 미니 영역
+export const MiniContainerFive = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 20vh;
+  width: 40vw;
+  background-color: grey;
+`;
+
+export const BoxEleven = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 18vw;
+  background-color: purple;
+`;
+
+export const BoxTwelve = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 18vw;
+  background-color: white;
+`;
+
+// 우측 하단 미니 영역
+export const MiniContainerSix = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 40vh;
+  width: 40vw;
+  background-color: pink;
+`;
+
+export const MiniContainerSeven = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 40vh;
+  width: 10vw;
+  background-color: orange;
+`;
+
+export const BoxSeventeen = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
+  background-color: pink;
+`;
+
+export const BoxTwentyFour = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
+  background-color: white;
+`;
+
+export const MiniContainerEight = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 40vh;
+  width: 10vw;
+  background-color: blue;
+`;
+
+export const MiniContainerNine = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 40vh;
+  width: 10vw;
+  background-color: yellow;
+`;
+
+export const BoxNineteen = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
+  background-color: pink;
+`;
+
+export const BoxTwentyFive = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 18vh;
+  width: 9vw;
+  background-color: white;
+`;
+
+export const MiniContainerTen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-around;
+  justify-content: space-around;
+  height: 40vh;
+  width: 10vw;
+  background-color: red;
 `;
