fe8abe55b068ec6545f84b3617c3998fdb818020
허성은 <soulll012@naver.com>
2022-11-08 22:21:34 +0900

    :recycle: refactor : email -> userId로 수정
    Former-commit-id: 665e5e7db40ae76eb30ca5f3953ba36d14f6a1e4

--- Backend/src/main/java/com/ssafy/ssantaClinic/api/controller/CalendarController.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/api/controller/CalendarController.java
@@ -30,7 +30,7 @@ import java.util.List;
  * @FileName : CalendarController
  * @Class 설명 : 어드벤트 캘린더 관련 요청을 처리하는 Controller
  */
-@Api(value = "어드벤트 캘린더 관련 API", tags = {"CalendarController"}, description = "어드벤트 캘린더 관련 컨트롤러")
+@Api(value = "어드벤트 캘린더 관련 API", tags = {"CalendarController"})
 @RestController
 @Slf4j
 @CrossOrigin(origins = "*")
@@ -38,7 +38,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CalendarController {
     private final CalendarService calendarService;
-    private final UserService userService;
     private final S3Service s3Service;
 
     @ApiOperation(value = "상자 목록 조회", notes = "오늘 열람가능한 상자 목록을 조회한다.")
@@ -49,14 +48,14 @@ public class CalendarController {
             @ApiResponse(code = 500, message = "서버 에러 발생")
     })
     @GetMapping
-    public ResponseEntity<?> getTodayBoxList(HttpServletRequest request) {
+    public ResponseEntity<?> getTodayBoxList() {
         /**
          * @Method Name : getTodayBoxList
          * @Method 설명 : 오늘 열람가능한 상자 목록을 조회한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
-        List<CalendarResponse.GetBoxResponse> boxes = calendarService.findAllTodayBoxes(email);
+        // 현재 로그인한 유저의 아이디 가져오기
+        int userId = JwtUtil.getCurrentUserId();
+        List<CalendarResponse.GetBoxResponse> boxes = calendarService.findAllTodayBoxes(userId);
         if(boxes.isEmpty())
             return ResponseEntity.noContent().build();
         else
@@ -71,15 +70,14 @@ public class CalendarController {
             @ApiResponse(code = 500, message = "서버 에러 발생")
     })
     @GetMapping(params = {"boxId"})
-    public ResponseEntity<?> getBox(HttpServletRequest request,
-                                    @RequestParam(value = "boxId") int boxId) {
+    public ResponseEntity<?> getBox(@RequestParam(value = "boxId") int boxId) {
         /**
          * @Method Name : getBox
          * @Method 설명 : 상자 상세 정보를 조회한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
-        CalendarResponse.GetBoxDetailResponse box = calendarService.findBox(email, boxId);
+        // 현재 로그인한 유저의 아이디 가져오기
+        int userId = JwtUtil.getCurrentUserId();
+        CalendarResponse.GetBoxDetailResponse box = calendarService.findBox(userId, boxId);
         return ResponseEntity.ok(box);
     }
 
@@ -89,14 +87,14 @@ public class CalendarController {
             @ApiResponse(code = 500, message = "서버 에러 발생")
     })
     @GetMapping("/play")
-    public ResponseEntity<?> playVoiceMessage(HttpServletRequest request, @RequestParam(value = "boxId") int boxId) throws Exception {
+    public ResponseEntity<?> playVoiceMessage(@RequestParam(value = "boxId") int boxId) throws Exception {
         /**
          * @Method Name : playVoiceMessage
          * @Method 설명 : 상자의 음성 메세지를 재생한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
-        calendarService.playAudio(email, boxId);
+        // 현재 로그인한 유저의 아이디 가져오기
+        int userId = JwtUtil.getCurrentUserId();
+        calendarService.playAudio(userId, boxId);
         return ResponseEntity.ok().build();
     }
 
@@ -121,8 +119,12 @@ public class CalendarController {
          * @Method Name : sendBox
          * @Method 설명 : 상자를 선물한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
+        // 현재 로그인한 유저의 아이디 가져오기
+        int currentUserId = JwtUtil.getCurrentUserId();
+        // 자기 자신에게는 선물 불가능
+        if(userId == currentUserId){
+            throw new CustomException(ErrorCode.SELF_GIFT_ERROR);
+        }
         // 오디오, 사진, 텍스트 모두 없을 시 오류
         if((boxRequest.getContent() == null || boxRequest.getContent().isBlank()) && audio == null && imges == null){
             throw new CustomException(ErrorCode.EMPTY_BOX_ERROR);
@@ -137,7 +139,7 @@ public class CalendarController {
         if(imges != null){
             imgUrls = s3Service.uploadImges(imges);
         }
-        AdventCalendar box = calendarService.saveBox(email, imgUrls, audioUrl, boxRequest);
+        AdventCalendar box = calendarService.saveBox(userId, imgUrls, audioUrl, boxRequest);
         return ResponseEntity.created(URI.create("/"+box.getId())).build();
     }
 
@@ -148,15 +150,12 @@ public class CalendarController {
             @ApiResponse(code = 500, message = "서버 에러 발생")
     })
     @GetMapping(params = {"userId"})
-    public ResponseEntity<?> getAdventCalendarInfo(HttpServletRequest request,
-                                     @RequestParam(value = "userId") int userId) {
+    public ResponseEntity<?> getAdventCalendarInfo(@RequestParam(value = "userId") int userId) {
         /**
          * @Method Name : getAdventCalendarInfo
          * @Method 설명 : 회원의 어드벤트 캘린더 정보를 조회한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
-        List<CalendarResponse.GetCalendarResponse> calendarInfo = calendarService.findAdventCalendarByUserId(email);
+        List<CalendarResponse.GetCalendarResponse> calendarInfo = calendarService.findAdventCalendarByUserId(userId);
         return ResponseEntity.ok(calendarInfo);
     }
 
@@ -169,15 +168,14 @@ public class CalendarController {
             @ApiResponse(code = 500, message = "서버 에러 발생")
     })
     @GetMapping(params = {"date"})
-    public ResponseEntity<?> getBoxListByDate(HttpServletRequest request,
-                                                   @RequestParam(value = "date") String date) {
+    public ResponseEntity<?> getBoxListByDate(@RequestParam(value = "date") String date) {
         /**
          * @Method Name : getBoxListByDate
          * @Method 설명 : 해당 날짜의 회원의 어드벤트 캘린더 정보를 조회한다.
          */
-        // 현재 로그인한 유저의 이메일 가져오기
-        String email = JwtUtil.getCurrentUserEmail().isPresent() ? JwtUtil.getCurrentUserEmail().get() : "anonymousUser";
-        List<CalendarResponse.GetBoxResponse> boxes = calendarService.findAllBoxesByDate(email, date);
+        // 현재 로그인한 유저의 아이디 가져오기
+        int userId = JwtUtil.getCurrentUserId();
+        List<CalendarResponse.GetBoxResponse> boxes = calendarService.findAllBoxesByDate(userId, date);
         if(boxes.isEmpty())
             return ResponseEntity.noContent().build();
         else
