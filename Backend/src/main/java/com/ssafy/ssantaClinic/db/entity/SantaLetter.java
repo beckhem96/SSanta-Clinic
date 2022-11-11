c4b196a0258217fba1d4f835e6c8b238ec4aa5b1
seung <ksgg1navercom@gmail.com>
2022-11-11 16:53:09 +0900

    ✨ feat: Clova 문장 감정 분석 기능 추가
    Former-commit-id: b662f03247b5979472e26b9e4aa4b84143a76825

--- Backend/src/main/java/com/ssafy/ssantaClinic/db/entity/SantaLetter.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/db/entity/SantaLetter.java
@@ -1,11 +1,14 @@
 package com.ssafy.ssantaClinic.db.entity;
 
+import com.ssafy.ssantaClinic.db.entity.columnEnum.LetterType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -18,10 +21,13 @@ public class SantaLetter {
     @Column(name = "santa_letter_id")
     private int id;
 
-    private String category;
+    @NotNull(message = "편지 타입은 필수 입니다.")
+    private LetterType type;
 
+    @NotBlank(message = "편지 제목이 필요합니다.")
     private String title;
 
+    @NotBlank(message = "편지 내용은 필수 입니다.")
     private String content;
 
 }
