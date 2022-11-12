96f4c8b2d9b17c98e33558c60445de67f202aa82
seung <ksgg1navercom@gmail.com>
2022-11-12 23:58:21 +0900

    ✨ feat: 명언 데이터 추가
    Former-commit-id: cfe3f7e6a641680b82566b2b6cc531ddca368a8f

--- Backend/src/main/java/com/ssafy/ssantaClinic/db/entity/Quote.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/db/entity/Quote.java
@@ -1,5 +1,22 @@
 package com.ssafy.ssantaClinic.db.entity;
 
-public class Quote {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quote_letter_id")
+    private int QuoteLetterId;
+
+    private String quote;
+
+    private String source;
 }
