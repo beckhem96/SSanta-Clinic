1b8a04c84dfd5d8166c2d291b04ee8e23427b7aa
yoonkyu <newjeff@naver.com>
2022-11-13 15:14:00 +0900

    :sparkles: feat : room에서 axios
    Former-commit-id: ef21e167467b3d645490e3e4822caf14ee96550d

--- Frontend/ssantaclinic/src/components/room/RoomModal.tsx
+++ Frontend/ssantaclinic/src/components/room/RoomModal.tsx
@@ -1,10 +1,32 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { MyRoomDiv, Wrapper, CanvasContainer } from './styled';
 import { RoomThree } from '../../three/RoomThree';
+import axios from 'axios';
+import { selectUserId, selectUserNickname } from '../../store/store';
+import { useRecoilValue } from 'recoil';
 
 export default function RoomModal(props: any) {
   const { onClose } = props;
   const items: number[] = [];
+  const NICKNAME = useRecoilValue(selectUserNickname);
+  const ID = useRecoilValue(selectUserId);
+  const TOKEN = localStorage.getItem('jwt') || '';
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/room/' + ID, {
+        headers: {
+          Authorization: TOKEN,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err.resonse);
+      });
+  }, []);
+
   for (let i = 1; i < 25; i++) {
     items.push(1);
   }
