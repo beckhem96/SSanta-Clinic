71adf67a4f01b957a6d3ee255216af62519d935c
yoonkyu <newjeff@naver.com>
2022-11-15 20:41:55 +0900

    :lipstick: style : 캘린더 alert design
    Former-commit-id: 48b92a8f1cf2d1edb91ad41fae3abe51de4b639c

--- Frontend/ssantaclinic/src/components/room/calendaralert/Calendar.tsx
+++ Frontend/ssantaclinic/src/components/room/calendaralert/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Div } from './style';
+import { CalendarAlertDiv, CalendarButton } from './style';
 
 import { CalendarModal } from '../../calendar/CalendarModal';
 
@@ -15,10 +15,10 @@ export function CalendarAlert(props: any) {
 
   if (!isHelp) {
     return (
-      <Div className="calendar">
-        캘린더 구경할래?
-        <button onClick={help}>ㅇㅋ</button>
-      </Div>
+      <CalendarAlertDiv className="calendar">
+        선물이 많이 왔을까?
+        <CalendarButton onClick={help}>구경</CalendarButton>
+      </CalendarAlertDiv>
     );
   } else {
     return <CalendarModal onClose={setisHelp}></CalendarModal>;
