e8af8d10062f16e665939f041c8939cd52a76d66
Your Name <beckhem96@naver.com>
2022-11-16 13:32:04 +0900

    :sparkles: feat: 산타 답장 보기 추가
    Former-commit-id: 8c3a69e4331ec9fc916d1057a0795c1894dfe1e2

--- Frontend/ssantaclinic/src/components/main/letter/LetterAlert.tsx
+++ Frontend/ssantaclinic/src/components/main/letter/LetterAlert.tsx
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Div, TitleLetterContainer, ButtonLetterContainer } from './styles';
 import { LetterList } from '../../letter/LetterList';
 import { WriteLetterPage } from '../../../pages/letter/WriteLetterPage';
 import { ReceiveLetterPage } from '../../../pages/letter/ReceiveLetterPage';
 import { motion } from 'framer-motion';
+import axios from 'axios';
+
+export interface ILetters {
+  title: string;
+  message: string;
+  read: boolean;
+  replyLetterId: number;
+  sendLetterId: number;
+}
 export function LetterAlert() {
+  const ACCESS_TOKEN = localStorage.getItem('jwt') || '';
   const [isList, setIsList] = useState<boolean>(true);
   const [isWrite, setIsWrite] = useState<boolean>(false);
   const [isReceive, setIsReceive] = useState<boolean>(false);
   const [letterId, setLetterId] = useState<number>(0);
+  const [letters, setLetters] = useState<any[]>([]);
+
+  // useEffect(() => {
+  //   console.log(letters, '편지들');
+  //   getLetters();
+  // }, []);
+  // function getLetters() {
+  //   axios
+  //     .get('http://localhost:8080' + '/api/letter', {
+  //       headers: {
+  //         Authorization: ACCESS_TOKEN,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       const data = res.data.reply;
+  //       for (let i = 0; i < data.length; i++) {
+  //         letters.push({
+  //           replyLetterid: data[i].replyLetterId,
+  //           sendLetterId: data[i].sendLetterId,
+  //           title: data[i].title,
+  //           message: data[i].message,
+  //           isRead: data[i].isRead,
+  //         });
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.response);
+  //     });
+  // }
   function Write() {
     setIsList(!isList);
     setIsWrite(!isWrite);
@@ -28,6 +67,7 @@ export function LetterAlert() {
           <h1>산타편지</h1>
         </TitleLetterContainer>
         <LetterList
+          // onLetters={set}
           onLetterId={setLetterId}
           onReceiveLetter={setIsReceive}
           onLetterList={setIsList}
@@ -64,6 +104,7 @@ export function LetterAlert() {
     return (
       <Div className="letterAlert">
         <ReceiveLetterPage
+          onLetterId={letterId}
           onBack={setIsReceive}
           goList={setIsList}
         ></ReceiveLetterPage>
