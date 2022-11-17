341106babea471fda12ba3b1623e4741c9922658
김혜린 <herrine.kim@gmail.com>
2022-11-18 00:33:28 +0900

    ✨ feat: boxDetail 모달 CSS(이미지 제외) 구현
    Former-commit-id: 72a0d1fbbdc1ed572d449b58fd0ad5809df17d14

--- Frontend/ssantaclinic/src/components/calendar/calendarDetail/styles.tsx
+++ Frontend/ssantaclinic/src/components/calendar/calendarDetail/styles.tsx
@@ -48,36 +48,36 @@ export const DayDiv = styled.div`
 `;
 
 export const CloseButton = styled.button`
-background-color: #fbeee0;
-border: 2px solid #422800;
-border-radius: 30px;
-box-shadow: #422800 4px 4px 0 0;
-color: #422800;
-cursor: pointer;
-display: inline-block;
-font-weight: 600;
-font-size: 1.5rem;
-text-align: center;
-text-decoration: none;
-user-select: none;
--webkit-user-select: none;
-touch-action: manipulation;
-font-family: 'Cafe24Ssurround';
-margin-top: 0;
-width: 5%;
-}
+  background-color: #fbeee0;
+  border: 2px solid #422800;
+  border-radius: 30px;
+  box-shadow: #422800 4px 4px 0 0;
+  color: #422800;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 600;
+  font-size: 1.5rem;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  font-family: 'Cafe24Ssurround';
+  margin-top: 0;
+  width: 5%;
+  }
 
-:hover {
-background-color: #fff;
-}
+  :hover {
+  background-color: #fff;
+  }
 
-:active {
-box-shadow: #422800 2px 2px 0 0;
-transform: translate(2px, 2px);
-}
+  :active {
+  box-shadow: #422800 2px 2px 0 0;
+  transform: translate(2px, 2px);
+  }
 
-@media (min-width: 100px) {
-{
-  min-width: 60px;
-}
+  @media (min-width: 100px) {
+  {
+    min-width: 60px;
+  }
 `;
