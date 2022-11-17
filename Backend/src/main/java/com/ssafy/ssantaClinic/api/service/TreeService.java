d5b130f99d8717715d8bb903f4fadb8a54f2fe13
허성은 <aysel0230@gmail.com>
2022-11-17 11:04:05 +0900

    :bug: fix : 트리 꾸미고 유저 보유 아이템 목록 업데이트
    Former-commit-id: aa6ede31b1cf75156d7e52f430a00f2445e8d9ee

--- Backend/src/main/java/com/ssafy/ssantaClinic/api/service/TreeService.java
+++ Backend/src/main/java/com/ssafy/ssantaClinic/api/service/TreeService.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface TreeService {
     String saveTreeImage(int userId, String treeUrl);
     List<String> getRandomTree(int userId);
+    void updateUserItemBox(int userId, String leftItem);
 }
