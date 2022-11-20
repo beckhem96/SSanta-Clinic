3128077647fb735950160436a4ae5edf3655834d
yoonkyu <newjeff@naver.com>
2022-11-20 11:18:04 +0900

    :sparkles: feat : room 설명
    Former-commit-id: 79831192471983b5f78e91fb6173ba6c03599add

--- Frontend/ssantaclinic/src/components/room/styled.tsx
+++ Frontend/ssantaclinic/src/components/room/styled.tsx
@@ -21,7 +21,59 @@ export const CanvasContainer = styled.div`
   box-shadow: 10px rgba(0, 0, 0, 0.2);
 `;
 
+export const DisContainer = styled.div`
+  position: absolute;
+
+  background: #ffffff;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.5;
+  border-radius: 10px;
+
+  box-shadow: 10px rgba(0, 0, 0, 0.2);
+`;
+
 export const CloseButton = styled.button`
+background-color: #fbeee0;
+border: 2px solid #422800;
+border-radius: 30px;
+box-shadow: #422800 4px 4px 0 0;
+color: #422800;
+cursor: pointer;
+display: inline-block;
+font-weight: 600;
+font-size: 1.5rem;
+padding: 0 18px;
+line-height: 50px;
+text-align: center;
+text-decoration: none;
+user-select: none;
+-webkit-user-select: none;
+touch-action: manipulation;
+z-index: 999;
+font-family: 'Cafe24Ssurround';
+}
+
+:hover {
+background-color: #fff;
+}
+
+:active {
+box-shadow: #422800 2px 2px 0 0;
+transform: translate(2px, 2px);
+}
+
+@media (min-width: 100px) {
+{
+  min-width: 60px;
+  padding: 0 25px;
+}
+`;
+
+export const DisButton = styled.button`
+left: 30px;
+top:30px;
 background-color: #fbeee0;
 border: 2px solid #422800;
 border-radius: 30px;
