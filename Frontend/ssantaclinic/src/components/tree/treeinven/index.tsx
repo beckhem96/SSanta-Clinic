58346c62ac8cbc152f23c4ae83959a199e34cb5d
yoonkyu <newjeff@naver.com>
2022-11-21 13:31:56 +0900

    :hammer: chore : 파일 삭제
    Former-commit-id: ac9bd8533f34406c3d8cc5b4741723aa10557003

--- Frontend/ssantaclinic/src/components/tree/treeinven/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { TreeInvenDiv } from './style';
-
-export function TreeInven() {
-  // 유저가 갖고 있는 아이템 정보 필요
-
-  console.log('TreeInven : ');
-  return <TreeInvenDiv>asdf</TreeInvenDiv>;
-}
